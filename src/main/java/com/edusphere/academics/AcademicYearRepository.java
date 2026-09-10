f33742b2028cc5c1dc0a236496b39c77ffc806b2
AmitKumarPrasad <39097317+AmitKumarPrasad@users.noreply.github.com>
2026-09-10 22:07:41 +0530

    Add academic year repository

---
+++ src/main/java/com/edusphere/academics/AcademicYearRepository.java
@@ -0,0 +1,9 @@
+package com.edusphere.academics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.UUID;
+
+public interface AcademicYearRepository extends JpaRepository<AcademicYear, UUID> {
+    List<AcademicYear> findBySchoolIdOrderByStartsOnDesc(UUID schoolId);
+}
