c571e0a6e5a41935a059ba5bce4faab2a22f3a15
AmitKumarPrasad <39097317+AmitKumarPrasad@users.noreply.github.com>
2026-09-11 12:33:32 +0530

    feat: configure S3 document storage client

---
+++ src/main/java/com/edusphere/documents/S3StorageConfig.java
@@ -0,0 +1,17 @@
+package com.edusphere.documents;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@ConditionalOnProperty(name = "app.storage.provider", havingValue = "s3")
+public class S3StorageConfig {
+    @Bean(destroyMethod = "close")
+    S3Client s3Client(@Value("${app.storage.s3.region:ap-south-1}") String region) {
+        return S3Client.builder().region(Region.of(region)).build();
+    }
+}
